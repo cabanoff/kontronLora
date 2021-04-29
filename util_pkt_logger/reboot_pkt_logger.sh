324e2043dff9e3ac431dda58d8c14ad91b7b6894
cabanoff <al.al.kabanov@gmail.com>
2021-04-29 21:38:00 +0200

    first commit

---
+++ util_pkt_logger/reboot_pkt_logger.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo /home/kontron/ver31/lora_gateway/util_pkt_logger/start_pkt_logger.sh
---
+++ util_pkt_logger/start_pkt_logger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/kontron/ver31/lora_gateway/util_pkt_logger
+xterm -e ./util_pkt_logger
+
+#while true
+#do
+#	if ! pgrep -x "MQTTSSLTest" > /dev/null
+#	then 	
+#		xterm -e ./MQTTSSLTest 
+#		sleep 10
+#	fi
+#done
